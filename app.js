60532eaf98c61ac99e3f20a0ed76826ac645dd19
Vaibhav Barala <vaibhavbarala8@gmail.com>
2024-10-29 11:41:17 +0530

    seconwd

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 1042;
 app.use(express.json())
 app.use(cors({
     origin:"http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials:true,
 }))
 app.use(cookieParser());
